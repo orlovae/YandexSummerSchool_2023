884ec31b21a57bf6adae56eecb9eb6690b928277
Alexander Orlov <79202875363@ya.ru>
2023-07-05 14:32:51 +0300

    release TaskA2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,5 @@
  * Для компилятора яндекса надо копировать main и нужный class
  */
 fun main(args: Array<String>) {
-    val taskE = TaskE1()
-    taskE.printAnswer()
+    val task = TaskA2()
 }
---
+++ src/main/kotlin/TaskA2.kt
@@ -0,0 +1,57 @@
+/**
+ * Вам дана последовательность измерений некоторой величины. Требуется определить, повторялось ли какое-либо число,
+ * причём расстояние между повторами не превосходило k.
+ * Формат ввода
+ * В первой строке задаются два числа n и k (1 ≤ n, k ≤ 10e5).
+ * Во второй строке задаются n чисел, по модулю не превосходящих 10e9.
+ * Формат вывода
+ * Выведите YES, если найдется повторяющееся число и расстояние между повторами не превосходит k и NO в противном
+ * случае.
+ *
+ * Пример 1
+ * 4 2
+ * 1 2 3 1
+ * Вывод
+ * NO
+ * Пример 2
+ * 4 1
+ * 1 0 1 1
+ * Вывод
+ * YES
+ * Пример 3
+ * 6 2
+ * 1 2 3 1 2 3
+ * Вывод
+ * NO
+ */
+class TaskA2 {
+    private val scan = java.util.Scanner(System.`in`)
+    private val inputFirst = scan.nextLine()
+        .split(" ")
+        .map { it.toInt() }
+    private val inputSecond = scan.nextLine()
+        .split(" ")
+        .map { it.toInt() }
+    private val requirement = inputFirst[1]
+
+    init {
+        print(
+            getAnswer(inputSecond)
+        )
+    }
+
+    private fun getAnswer(input: List<Int>): String {
+        val map = mutableMapOf<Int, Int>()
+        input.forEachIndexed { index, it ->
+            val diff = map[it]?.let { value ->
+                index - value
+            }
+
+            if (diff != null && diff == requirement) {
+                return ("YES")
+            }
+            map[it] = index
+        }
+        return ("NO")
+    }
+}
