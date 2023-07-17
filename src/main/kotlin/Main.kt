2c109ec68fb468993c76d527e5e5545b157af066
Alexander Orlov <79202875363@ya.ru>
2023-07-17 10:25:49 +0300

    Add Task DeepDrive 2023, no solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
+import java.util.concurrent.atomic.AtomicInteger
+
 /**
  * Для компилятора яндекса надо копировать main и нужный class
  */
 fun main(args: Array<String>) {
-    val task = TaskB0()
+    val task = TaskDeepDriveI1()
 }
