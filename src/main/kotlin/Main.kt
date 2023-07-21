da0a999eba3873327c0007a9a16805737ddf5613
Alexander Orlov <79202875363@ya.ru>
2023-07-21 08:21:26 +0300

    TaskB5 no solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,5 @@
  * Для компилятора яндекса надо копировать main и нужный class
  */
 fun main(args: Array<String>) {
-    val task = TaskA5Solution()
+    val task = TaskB5()
 }
---
+++ src/main/kotlin/TaskB5.kt
@@ -0,0 +1,58 @@
+/**
+Дана строка s и число k. В строке s требуется найти подстроку максимальной длины, в которой все различные символы
+встречаются не менее k раз.
+
+Формат ввода
+Первая строка содержит через пробел два целых числа n и k (1≤n,k≤2x10e5) — длину строки s и минимальное количество
+каждого из символов в искомой подстроке. Вторая строка содержит строку s (|s|= n), состоящую из строчных латинских
+букв.
+
+Формат вывода
+Выведите одно число — максимальную длину подстроки, в которой все различные символы встречаются не менее k раз.
+Пример 1
+Ввод
+5 3
+aaabb
+Вывод
+3
+Пример 2
+Ввод
+6 2
+ababbc
+Вывод
+5
+ */
+class TaskB5 {
+    private val firstString = readln().trim().split(" ").map { it.toInt() }
+    private val length = firstString[0]
+    private val k = firstString[1]
+    private val string = readln().trim()
+    private val map = mutableMapOf<Char, Int>()
+
+    init {
+        var answer = 0
+        var previous = 0
+        var current = 1
+        var next = 2
+        while (next <= string.lastIndex) {
+            addMap(string[previous])
+
+            if (string[current] == null && string[next] == null){
+
+            }
+
+        }
+
+        println(answer)
+
+    }
+
+    private fun addMap(item: Char) {
+        if (map[item] != null) {
+            map[item] = map[item]!! + 1
+        } else {
+            map[item] = 1
+        }
+
+    }
+}
